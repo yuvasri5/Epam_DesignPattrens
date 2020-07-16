f0d1ecb255ab7acb994fcfe30ffb6ac0f1cabb27
yuvasri5 <60392326+yuvasri5@users.noreply.github.com>
2020-07-16 12:07:27 +0530

    Create Circle.java

---
+++ Circle.java
@@ -0,0 +1,14 @@
+public class Circle
+{
+    double radius;
+
+    Circle(double radius)
+    {
+        this.radius = radius;
+    }
+
+    double getArea()
+    {
+        return (22.0/7.0) * radius * radius;
+    }
+}
